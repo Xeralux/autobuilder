d06638931773fb0f4b6cabaae5c3622975abe03f
Matt Madison <matt@madison.systems>
2018-08-05 09:44:41 -0700

    Maybe need more defer, not less

--- autobuilder/message_utils.py
+++ autobuilder/message_utils.py
@@ -3,23 +3,24 @@ from twisted.internet import defer
 from twisted.python import log
 
 
+@defer.inlineCallbacks
 def getChangesForSourceStamps(master, sslist):
     log.msg("getChangesForSourceStamps: sslist=%s" % sslist)
     changelist = []
     for ss in sslist:
-        log.msg('SOURCESTAMP: %s' % ss)
-        changes = master.data.get(("sourcestamps", ss['ssid'], "changes"))
+        changes = yield master.data.get(("sourcestamps", ss['ssid'], "changes"))
         log.msg('CHANGES: %s' % changes)
         changelist += changes
-    return changelist
+    return defer.returnValue(changelist)
 
 
 class AutobuilderMessageFormatter(MessageFormatter):
+    @defer.inlineCallbacks
     def buildAdditionalContext(self, master, ctx):
         ctx.update(self.ctx)
         log.msg("buildAdditionalContext: orig context=%s" % ctx)
         if ctx['sourcestamps']:
-            ctx['changes'] = getChangesForSourceStamps(master, ctx['buildset']['sourcestamps'])
+            ctx['changes'] = yield getChangesForSourceStamps(master, ctx['buildset']['sourcestamps'])
         else:
             ctx['changes'] = []
         log.msg("buildAdditionalContext: set changes to: %s" % ctx['changes'])
