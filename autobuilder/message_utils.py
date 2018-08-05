cb43a76c443d4e3aedec3795b1ff9e9bb53df597
Matt Madison <matt@madison.systems>
2018-08-05 07:36:33 -0700

    Another shot at get changes information

--- autobuilder/message_utils.py
+++ autobuilder/message_utils.py
@@ -4,22 +4,24 @@ from twisted.internet import defer
 
 # noinspection PyPep8Naming
 @defer.inlineCallbacks
-def getChangesForBuild(master, buildid):
-    dl = [master.data.get(("builds", buildid, "changes"))]
-    changesd = yield defer.gatherResults(dl)
+def getChangesForSourceStamps(master, sslist):
     changes = []
-    for c in changesd:
-        change = {'author': c['author'],
-                  'comments': c['comments'],
-                  'files': c['files'],
-                  'revlink': c['revlink'],
-                  'revision': c['revision']
-                  }
-        changes.append(change)
+    for ss in sslist:
+        changesd = master.data.get(("sourcestamps", ss['id'], "changes"))
+        sourcestampd = master.data.get("sourcestamps", ss['id'])
+        changes, sourcestamp = yield defer.gatherResults([changesd, sourcestampd])
+        for c in changesd:
+            change = {'author': c['author'],
+                      'comments': c['comments'],
+                      'revlink': c['revlink'],
+                      'revision': c['revision']
+                      }
+            changes.append(change)
     defer.returnValue(changes)
 
 
 class AutobuilderMessageFormatter(MessageFormatter):
     def buildAdditionalContext(self, master, ctx):
         ctx.update(self.ctx)
-        ctx['changes'] = getChangesForBuild(master, ctx['build']['buildid'])
+        if ctx['sourcestamps']:
+            ctx['changes'] = getChangesForSourceStamps(master, ctx['buildset']['sourcestamps'])
